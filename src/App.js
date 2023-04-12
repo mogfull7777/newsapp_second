17e0595a3f1c8e9ca51117181d98e7a2349a5e6f
mogfull7777 <anjunyoung94@gmail.com>
2023-04-12 22:48:59 +0900

    useState and display view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+
+    const [memo, setMemo] = useState("");
+
+    const changeMemo = () => {
+        setMemo("World")
+    }
+
+
     return (
         <div>
-          <h1>Hello again!!!</h1>
+            <h1>{memo}</h1>
+            <button onClick={changeMemo}>change</button>
         </div>
     );
 };
